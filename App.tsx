0938846490183d08caaf146cc4c59f00d4005aa8
iiy4383 <znlspf123@naver.com>
2021-07-20 17:15:46 +0900

    View and Text, Styled Component

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('Lee');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hi there! my name is {name}.</Text>
       <StatusBar style="auto" />
+      <View>
+        <Button title='update Name' color="red" onPress={() => setName('Leejong')}/>
+      </View>
     </View>
   );
 }
@@ -14,7 +19,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
