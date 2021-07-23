8da87a27a0585ad4bbd34955f1aa37d3b3872d55
iiy4383 <znlspf123@naver.com>
2021-07-23 14:05:21 +0900

    review app init

--- App.tsx
+++ App.tsx
@@ -1,57 +1,15 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, FlatList, TouchableWithoutFeedback, Keyboard } from 'react-native';
-import AddTodo from './components/addTodo';
-import Header from "./components/header";
-import TodoLists from './components/todoLists';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
 
 export default function App(): JSX.Element {
-  const [todos, setTodos] = useState([
-    {
-      text: "Hey!",
-      key: '1'
-    }
-  ]);
-
-  const onTouchablePress = (key: string): void => setTodos(() => todos.filter(ele => ele.key !== key));
-  const onAddPress = (text: string): void => setTodos((prev) => [
-    {
-      text,
-      key: (prev.length + 1).toString(),
-    },
-    ...prev
-  ]);
 
   return (
-    <TouchableWithoutFeedback onPress={() => {
-      Keyboard.dismiss();
-    }}>
-      <View style={styles.container}>
-        <Header />
-        <View style={styles.content}>
-          <AddTodo pressHandler={onAddPress} />
-          <View style={styles.list}>
-            <FlatList
-              data={todos}
-              renderItem={({item}): JSX.Element => (
-                <TodoLists item={item} pressHandler={onTouchablePress}/>
-              )}
-            />
-          </View>
-        </View>
-      </View>
-    </TouchableWithoutFeedback>
+    <View style={styles.container}>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: 'white',
-  },
-  content: {
-    paddingHorizontal: 20,
-  },
-  list: {
-    paddingHorizontal: 10,
   },
 });
